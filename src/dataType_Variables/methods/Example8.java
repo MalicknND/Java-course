9a2eb1e9439112fc894a48687c990321985643f9
MalicknND <ndiayemalick062@gmail.com>
2025-05-02 14:33:17 +0200

    Methods

--- src/dataType_Variables/methods/Example8.java
+++ src/dataType_Variables/methods/Example8.java
@@ -1,4 +1,17 @@
 package dataType_Variables.methods;
 
 public class Example8 {
+    public static void main(String[] args) {
+        int factorial0f5 = factorial(5);
+        System.out.println("Factorial of 5 is: " + factorial0f5);
+    }
+
+    // Factorial of a number n is defined as n! = n * (n-1) * (n -2) * ... * 1
+    public static int factorial(int n) {
+        if(n == 0){
+            return 1;
+        }else {
+            return n * factorial(n-1);
+        }
+    }
 }
--- src/dataType_Variables/methods/Exemple9.java
+++ src/dataType_Variables/methods/Exemple9.java
@@ -1,4 +1,16 @@
 package dataType_Variables.methods;
 
+// varargs in the methos
 public class Exemple9 {
+    public static void main(String[] args) {
+        printNumbers();
+        printNumbers(5);
+        printNumbers(1, 2, 3, 4, 5);
+    }
+
+    public static void printNumbers(int... numbers) {
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
 }
